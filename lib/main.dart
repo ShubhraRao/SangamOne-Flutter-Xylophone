390d5339969c5e20c8f5b9d4a2d789f81361e4b3
Shubhra Rao Kuthyar <shubhrakuthyar29@gmail.com>
2021-02-12 09:54:38 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'xylo.dart';
+
+void main(){
+  runApp(MaterialApp(home: Xylophone()));
+}
---
+++ lib/xylo.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+class Xylophone extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    Widget keys(Color color, String filename) {
+    return FlatButton(
+      minWidth: MediaQuery.of(context).size.width,
+      color: color, onPressed: () {
+      final player = AudioCache();
+      player.play(filename);
+    });
+  }
+    return Scaffold(
+        body: Column(children: <Widget>[
+      keys(Colors.red, "note1.wav"),
+      keys(Colors.orange, "note2.wav"),
+      keys(Colors.yellow, "note3.wav"),
+      keys(Colors.green, "note4.wav"),
+      keys(Colors.blue, "note5.wav"),
+      keys(Colors.indigo, "note6.wav"),
+      keys(Colors.purple, "note7.wav"),
+    ]));
+  }
+} 
